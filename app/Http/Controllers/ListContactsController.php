6896d83e68dec726841326fc4a42a6dcc1603b53
Antonio de Nardin <antoniodenardin01@gmail.com>
2025-07-04 15:38:12 -0300

    Revert "front end"
    This reverts commit b9e933dfc34abc445efcd5f9a40b291582908395.

--- app/Http/Controllers/ListContactsController.php
+++ app/Http/Controllers/ListContactsController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Inertia\Inertia;
 use App\Models\Contact;
 
 class ListContactsController extends Controller
@@ -10,8 +9,6 @@ class ListContactsController extends Controller
     public function __invoke()
     {
         $contacts = Contact::paginate(10);
-        return Inertia::render('Contacts/Index', [
-            'contacts' => $contacts,
-        ]);
+        return view('contacts.index', compact('contacts'));
     }
 }
